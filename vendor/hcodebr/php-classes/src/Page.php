bb6600ac4a1325abb4da26b196c49c0b728751b9
maurici3 <carusojr@outlook.com>
2022-03-18 17:20:11 -0300

    chore: added page class

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page 
+{
+
+  private $tpl;
+  private $options = [];
+  private $defaults = [
+    "data"=>[]
+  ];
+
+  public function __construct($opts = array()) 
+  {
+
+    $this->options = array_merge($this->defaults, $opts);
+
+    $config = array(
+      "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+      "cache_dir"     => $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."cache".DIRECTORY_SEPARATOR,
+      "auto_escape"   => true,
+      "base_url" => null,
+      "debug"         => false // set to false to improve the speed
+    );
+
+    Tpl::configure($config);
+
+    $this->tpl = new Tpl;
+
+    $this->setData($this->options["data"]);
+
+    $this->tpl->draw("partials".DIRECTORY_SEPARATOR."header");
+
+  }
+
+  private function setData($data = array())
+  {
+
+    foreach ($data as $key => $value) {
+      $this->tpl->assign($key, $value);
+    }
+
+  }
+
+  public function setTpl($name, $data = array(), $returnHTML = false) 
+  {
+    
+    $this->setData($data);
+
+    return $this->tpl->draw($name, $returnHTML);
+
+  }
+
+  public function __destruct() 
+  {
+    
+    $this->tpl->draw("partials".DIRECTORY_SEPARATOR."footer");
+
+  }
+
+}
+
+?>
