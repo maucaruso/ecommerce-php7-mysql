846b59d997672b1c5d1371fbc289d70f9930958e
maurici3 <carusojr@outlook.com>
2022-03-19 22:48:48 -0300

    feat: admin categories page

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -207,6 +208,93 @@ $app->post("/admin/forgot/reset", function() {
 	$page = new PageAdmin(["header"=>false,"footer"=>false]);
 
 	$page->setTpl("recovery-password".DIRECTORY_SEPARATOR."forgot-reset-success");
+
+});
+
+$app->get("/admin/categories", function (){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories".DIRECTORY_SEPARATOR."categories", [
+		"categories"=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function () {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories".DIRECTORY_SEPARATOR."categories-create");
+
+});
+
+$app->post("/admin/categories/create", function (){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function ($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function ($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories".DIRECTORY_SEPARATOR."categories-update", [
+		"category" =>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function ($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
 
 });
 
