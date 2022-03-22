9d264fa80c04a08b1b2607dba9e204034c719491
maurici3 <carusojr@outlook.com>
2022-03-22 13:54:47 -0300

    feat: reset password website

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -185,7 +185,7 @@ class User extends Model
 
   }
 
-  public static function getForgot($email)
+  public static function getForgot($email, $inadmin = true)
   {
 
     $sql = new Sql();
@@ -221,7 +221,15 @@ class User extends Model
           pack('a16', User::SECRET_IV)
         ));
 
-        $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+        if ($inadmin === true) {
+
+          $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+        } else {
+
+          $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+
+        }
 
         $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
           "name"=>$data["desperson"],
