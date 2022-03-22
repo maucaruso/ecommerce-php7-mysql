305f6c29a45743365036c6c5baac87ee32e061a3
maurici3 <carusojr@outlook.com>
2022-03-22 13:40:13 -0300

    feat: Tela de cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -318,6 +318,24 @@ class User extends Model
 
   }
 
+  public static function getErrorRegister()
+  {
+
+    $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+    User::clearErrorRegister();
+
+    return $msg;
+
+  }
+
+  public static function clearErrorRegister()
+  {
+
+    $_SESSION[User::ERROR_REGISTER] = NULL;
+
+  }
+
   public static function checkLoginExist($login)
   {
 
